8416656ce50fa49dfbb7cf4f35d86ecf16fbe6a4
Kien <92088660+KienDuc24@users.noreply.github.com>
2024-10-26 05:56:54 +0700

    up

---
+++ Ex09/app/src/main/java/tlu/edu/vn/ht63/ex09/MainActivity.java
@@ -0,0 +1,63 @@
+package tlu.edu.vn.ht63.ex09;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import tlu.edu.vn.ht63.ex09.R;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private Button btnPlay, btnStop;
+    private TextView tvSongTitle;
+    private static final String AUDIO_URL =
+            "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+    // Thay bằngURL bài hát của bạn
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnPlay = findViewById(R.id.btn_play);
+        btnStop = findViewById(R.id.btn_stop);
+        tvSongTitle = findViewById(R.id.songName);
+        btnPlay.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MusicService.class);
+            intent.putExtra("audioUrl", AUDIO_URL);
+            startService(intent);
+            tvSongTitle.setText("Đang phát: SoundHelix-Song-1.mp3");
+        });
+        btnStop.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MusicService.class);
+            stopService(intent);
+            tvSongTitle.setText("Dừng phát nhạc");
+        });
+
+        imageView = findViewById(R.id.vinyl);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+
+            private void startAnimation() {
+//                Animation animation = AnimationUtils.loadAnimation(this, R.anim.translate_animation);
+//                imageView.startAnimation(animation);
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopAnimation();
+            }
+
+            private void stopAnimation() {
+                imageView.clearAnimation(); // Ngừng hiệu ứng quay
+            }
+        });
+    }
+}
